b65b71b63863581a49cdd7908d3d0c559e016659
samanthajarrah <samantha.jarrah2@gmail.com>
2023-11-28 08:14:01 -0800

    moved create_json_str to main

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import json
 import re
 import click
 import requests
-
 from pdf_generator import build_recipe_pdf
-from json_str import create_json_str
 
 # clears the terminal
 click.clear()
@@ -452,6 +450,27 @@ def process_meal_type_input(meal_type_input, possible_meal_types):
             return None
 
     return meal_type_input
+
+def create_json_str(recipe_type, cuisine_types, diet_types, intolerances, include_ingredients, exclude_ingredients):
+    """Create json_str from recipe parameters"""
+
+    json_data = {}
+    if recipe_type:
+        json_data["query"] = recipe_type
+    if cuisine_types:
+        json_data["cuisine"] = cuisine_types
+    if diet_types:
+        json_data["diet"] = diet_types
+    if intolerances:
+        json_data["intolerances"] = intolerances
+    if include_ingredients:
+        json_data["includeIngredients"] = include_ingredients
+    if exclude_ingredients:
+        json_data["excludeIngredients"] = exclude_ingredients
+    json_data["instructionsRequired"] = "true"
+    json_data["fillIngredients"] = "true"
+    json_data["addRecipeInformation"] = "true"
+    return json.dumps(json_data)
 
 if __name__ == '__main__':
     start()
